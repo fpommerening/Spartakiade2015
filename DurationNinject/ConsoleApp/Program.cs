91fcc28f575781ca41c75fa2561a2f859462f9d3
Frank Pommerening <frank@pommerening-online.de>
2015-03-15 09:13:38 +0100

    Add Sample-Project DurationNinject

---
+++ DurationNinject/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FP.Spartakiade2015.DurationNinject.DAL;
+using Ninject;
+
+namespace FP.Spartakiade2015.DurationNinject.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IKernel kernel = new StandardKernel();
+                kernel.Bind<IUserRepository>().To<UserRepository>();
+
+                IUserRepository userRepo = kernel.Get<IUserRepository>();
+
+                foreach (var user in userRepo.GetAllUsers())
+                {
+                    Console.WriteLine("{0}:\t {1} {2}", user.Id, user.FirstName, user.Name);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            Console.ReadLine();
+        }
+    }
+}
