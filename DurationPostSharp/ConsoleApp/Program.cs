2e9ae748f21ed10076f79e97b98559597e9a412f
Frank Pommerening <frank@pommerening-online.de>
2015-03-15 14:17:46 +0100

    Add project DurationPostSharp

---
+++ DurationPostSharp/ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FP.Spartakiade2015.DurationPostSharp.DAL;
+using log4net;
+using Ninject;
+
+namespace FP.Spartakiade2015.DurationPostSharp.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            ILog logger = LogManager.GetLogger(typeof (Program));
+
+            try
+            {
+                IKernel kernel = new StandardKernel();
+                kernel.Bind<IUserRepository>().To<UserRepository>();
+
+                IUserRepository userRepo = kernel.Get<IUserRepository>();
+
+                foreach (var user in userRepo.GetAllUsers())
+                {
+                    Console.WriteLine("{0}:\t {1} {2}", user.Id, user.FirstName, user.Name);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DurationPostSharp/DAL/UserRepository.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FP.Spartakiade2015.DurationPostSharp.DAL
+{
+    public class UserRepository : IUserRepository
+    {
+        public UserRepository()
+        {
+            Users = new List<DOUser>
+            {
+                new DOUser
+                {
+                    Id = 1,
+                    Name = "Mustermann",
+                    FirstName = "Max",
+                    City = "Musterstadt",
+                    Street = "Hauptstraße 17",
+                    ZipCode = "12345"
+                },
+                new DOUser
+                {
+                    Id = 2,
+                    Name = "Meier",
+                    FirstName = "Sabine",
+                    City = "Musterdorf",
+                    Street = "Berlinstraße 42",
+                    ZipCode = "98765"
+                }
+            };
+        }
+
+        private List<DOUser> Users { get; set; }
+
+        public List<DOUser> GetAllUsers()
+        {
+            return Users;
+        }
+
+        public DOUser GetUserById(int userId)
+        {
+            return Users.FirstOrDefault(x => x.Id == userId);
+        }
+
+        public void SaveUser(DOUser userToSave)
+        {
+            if (userToSave.Id <= 0)
+            {
+                userToSave.Id = Users.Max(x => x.Id) + 1;
+            }
+            Users.RemoveAll(x => x.Id == userToSave.Id);
+            Users.Add(userToSave);
+        }
+
+        public void DeleteUser(int userId)
+        {
+            Users.RemoveAll(x => x.Id == userId);
+        }
+    }
+}
