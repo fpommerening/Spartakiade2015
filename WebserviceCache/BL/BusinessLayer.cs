de9537a8780133714add69a17ccbe8707b755ea8
Frank Pommerening <frank@pommerening-online.de>
2015-03-17 22:05:32 +0100

    Add sample project Webservice cache

---
+++ WebserviceCache/BL/BusinessLayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FP.Spartakiade2015.WebserviceCache.Contracts;
+
+namespace FP.Spartakiade2015.WebserviceCache.BL
+{
+    public class BusinessLayer
+    {
+        public decimal CheckAvailableCapacity(string customer, DateTime validFrom, DateTime validTo, decimal quantity,
+            CapacityUnit unit)
+        {
+            System.Threading.Thread.Sleep(1500);
+            return quantity/2;
+        }
+
+    }
+}
---
+++ WebserviceCache/Webservice/EnergyServiceImplementation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FP.Spartakiade2015.WebserviceCache.BL;
+using FP.Spartakiade2015.WebserviceCache.Contracts;
+
+namespace FP.Spartakiade2015.WebserviceCache.Webservice
+{
+    public class EnergyServiceImplementation : IBookCapacity_Binding
+    {
+        public BookCapacityResponseType BookCapacity(BookCapacityRequestType request)
+        {
+            var BL = new BusinessLayer();
+            var freeCapacity = BL.CheckAvailableCapacity(request.Customer, request.ValidFrom, request.ValidTo,
+                request.Quantity, request.Unit);
+            return new BookCapacityResponseType
+            {
+                MessageIdentifier = request.MessageIdentifier,
+                Quantity = freeCapacity,
+                Unit = request.Unit
+            };
+        }
+    }
+}
