edacaac78b6648a6e7e7714d3732eb0b2aa8ebb6
Frank Pommerening <frank@pommerening-online.de>
2015-03-18 21:24:10 +0100

    Add missing sample files

---
+++ WebserviceCache/Webservice/CustomerServiceImplementation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FP.Spartakiade2015.WebserviceCache.BL;
+using FP.Spartakiade2015.WebserviceCache.Contracts;
+
+namespace FP.Spartakiade2015.WebserviceCache.Webservice
+{
+    public class CustomerServiceImplementation : ICustomerInfo_Binding
+    {
+        public CustomerInfoResponseType CustomerInfo(CustomerInfoRequestType request)
+        {
+            var bl = new BusinessLayer();
+            var bo = bl.GetCustomerInfo(request.Name, request.FirstName, request.Street, request.ZipCode);
+
+            return new CustomerInfoResponseType
+            {
+                CustomerId = bo.Id,
+                FirstName = bo.FirstName,
+                Name = bo.Name,
+                ReliabilityIndex = bo.ReliabilityIndex,
+                Street = bo.Street,
+                Title = bo.Title,
+                ZipCode = bo.ZipCode
+            };
+        }
+    }
+}
---
+++ WebservicePersistence/TestConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace FP.Spartakiade2015.WebservicePersistence.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (BookCapacityService client = new BookCapacityService { Url = "http://localhost:49874/EnergyService.asmx" })
+                {
+                    
+                    var request = new BookCapacityRequestType
+                    {
+                        Customer = "Frank Pommerening",
+                        MessageIdentifier = Guid.NewGuid().ToString("D"),
+                        Quantity = 5000,
+                        Unit = CapacityUnit.KWh,
+                        ValidFrom = DateTime.Now.AddDays(1),
+                        ValidTo = DateTime.Now.AddMonths(1).AddDays(1)
+                    };
+                    Console.WriteLine("Starte Kapazitätsanfrage");
+                    var result = client.BookCapacity(request);
+                    Console.WriteLine("{0}: {1} {2}", result.MessageIdentifier, result.Quantity, result.Unit);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            Console.ReadLine();
+        }
+    }
+}
