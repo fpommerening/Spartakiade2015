9921390895bf3ace457e2d7c145d8d6bd2d57a43
Frank Pommerening <frank@pommerening-online.de>
2015-03-18 20:11:00 +0100

    Add simple cache to sample

---
+++ WebserviceCache/Webservice/ServiceCache.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FP.Spartakiade2015.WebserviceCache.Webservice
+{
+    public static class ServiceCache
+    {
+        private static readonly List<ServiceCacheItem> Items = new List<ServiceCacheItem>();
+
+        private static readonly object syncRoot = new object();
+
+        private static bool  cleanUpRunning;
+
+        public static object GetItem(string methodHash, string parameterHashCode)
+        {
+            ServiceCacheItem cachedItem;
+
+            lock (syncRoot)
+            {
+                cachedItem = Items.FirstOrDefault(x => x.MethodHash == methodHash && x.SerializedParameter == parameterHashCode &&
+                     x.UseUntilTimestamp > DateTime.Now);
+            }
+            
+           
+            return cachedItem == null ? null : cachedItem.Value;
+        }
+
+        public static void AddItem(string methodName, string serializedParameter, object value, TimeSpan cacheTimespan)
+        {
+            var item = new ServiceCacheItem
+            {
+                MethodHash = methodName,
+                SerializedParameter = serializedParameter,
+                UseUntilTimestamp = DateTime.Now.Add(cacheTimespan),
+                Value = value
+            };
+            lock (syncRoot)
+            {
+                Items.Add(item);
+            }
+            CleanUp();
+        }
+
+        private static void CleanUp()
+        {
+            if (cleanUpRunning) return;
+
+            cleanUpRunning = true;
+            Task.Factory.StartNew(() =>
+            {
+                while (Items.Any())
+                {
+                    lock (syncRoot)
+                    {
+                        Items.RemoveAll(x => x.UseUntilTimestamp < DateTime.Now);
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                }
+                cleanUpRunning = false;
+            });
+        }
+    }
+}
---
+++ WebserviceCache/Webservice/ServiceCacheItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FP.Spartakiade2015.WebserviceCache.Webservice
+{
+    public class ServiceCacheItem
+    {
+        public string MethodHash { get; set; }
+        public string SerializedParameter { get; set; }
+        public object Value { get; set; }
+        public DateTime UseUntilTimestamp { get; set; }
+    }
+}
